e6625be9f5e50bfd4eb8faf93396c51b4043f102
Fidel Becerra <60111311+FifiBB@users.noreply.github.com>
2020-01-22 12:59:29 -0700

    Create Program.cs

---
+++ clase_1/Practica1/Program.cs
@@ -0,0 +1,12 @@
+using System;
+// importamos la libreria principal.
+namespace practica_uno
+{
+    class program //clase basica
+    {
+        static void Main(string args)//void no regresa nada
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
